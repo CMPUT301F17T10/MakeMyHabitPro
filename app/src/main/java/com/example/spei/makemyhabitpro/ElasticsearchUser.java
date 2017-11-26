2b6e9c0d7d8c5131e30e2c58d2084276b9fec770
pshn111 <spei@ualberta.ca>
2017-11-25 17:33:04 -0700

    add ela

---
+++ app/src/main/java/com/example/spei/makemyhabitpro/ElasticsearchUser.java
@@ -0,0 +1,26 @@
+package com.example.spei.makemyhabitpro;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
+import com.searchly.jestdroid.JestDroidClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
+import android.util.Log;
+
+import static android.R.attr.text;
+import static android.R.id.message;
+/**
+ * Created by spei on 11/25/17.
+ */
+
+public class ElasticsearchUser {
+}
